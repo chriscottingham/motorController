1481514063e0f93dc3013d7be5fd32aa740e5653
Chris Cottingham <CCottingham@national.aaa.com>
2019-11-02 15:16:21 -0400

    gitignore

--- Debug/src/GpioMode.d
+++
@@ -1,6 +0,0 @@
-src/GpioMode.o: ../src/GpioMode.cpp ../include/GpioMode.h \
- ../include/TypedEnum.h
-
-../include/GpioMode.h:
-
-../include/TypedEnum.h:
--- Debug/src/Logger.d
+++
@@ -1,6 +0,0 @@
-src/Logger.o: ../src/Logger.cpp ../include/Logger.h \
- ../system/include/diag/Trace.h
-
-../include/Logger.h:
-
-../system/include/diag/Trace.h:
