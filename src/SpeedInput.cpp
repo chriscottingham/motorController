f80953cced54cae2e3c19882fe7a9a5d96bbbcdd
Chris Cottingham <CCottingham@national.aaa.com>
2019-11-16 01:47:08 -0500

    proper adc speed

--- src/SpeedInput.cpp
+++ src/SpeedInput.cpp
@@ -13,11 +13,12 @@ SpeedInput::SpeedInput(GPIO_TypeDef* gpio, uint8_t inputPin) :
 		gpio(gpio), speedState(1234) {
 	this->inputPin = inputPin;
 //	IoDriver::initPin(GPIOA, vector<uint8_t>{2}, GpioMode::analogInput);
+	GPIOA->CRL &= ~(0xf<<8);
 
 	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
 	ADC1->SMPR2 |= 0x1c;
 	ADC1->SQR3 |= inputPin;
-	ADC1->CR2 |= ADC_CR2_CONT | ADC_CR2_ADON;
+	ADC1->CR2 |= ADC_CR2_CONT | ADC_CR2_ADON | ADC_CR2_ALIGN;
 
 	ADC1->CR2 |= ADC_CR2_ADON;
 }
@@ -32,7 +33,8 @@ void SpeedInput::setMaxSpeed(uint16_t maxSpeed) {
 
 void SpeedInput::run() {
 	while (1) {
-		speedState.inputSpeed = (0xff & ADC1->DR) * maxSpeed / 256;
+		volatile long masked = (0xff00 & ADC1->DR) >> 8;
+		speedState.inputSpeed = masked * maxSpeed / 256;
 		stateHolder->set(speedState);
 
 		vTaskDelay(pdMS_TO_TICKS(50));
