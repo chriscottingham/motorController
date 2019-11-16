61721d8d425b5907d5a11651669a014f6771dfe7
Chris Cottingham <CCottingham@national.aaa.com>
2019-11-16 16:51:57 -0500

    pwm working

--- src/PwmControl.cpp
+++ src/PwmControl.cpp
@@ -11,7 +11,12 @@ PwmControl::PwmControl(GPIO_TypeDef* gpio, uint8_t outputPin) : gpio(gpio), outp
 
 	IoDriver::initPin(gpio, vector<uint8_t>({outputPin}), GpioMode::altPushPullOutput);
 
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+
 	TIM3->CCMR1 |= TIM_CCMR1_OC1M | TIM_CCMR1_OC1FE;
+	TIM3->CCR2 |= 0x70;
+	TIM3->ARR = 256;
+	TIM3->CCER |= 0x01;
 
 	TIM3->CR1 |= TIM_CR1_CEN;
 
@@ -31,6 +36,9 @@ void PwmControl::setMaxMotorRpm(long maxRpm) {
 void PwmControl::run() {
 
 	while (1) {
+		short desiredSpeedValue = desiredSpeed->get().rpm;
+		short currentSpeedValue = currentSpeed->get().rpm;
+		TIM3->CCR1 = 0xff * (desiredSpeedValue - currentSpeedValue) / desiredSpeedValue;
 		vTaskDelay(pdMS_TO_TICKS(100));
 	}
 }
